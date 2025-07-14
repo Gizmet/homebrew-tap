eabbece1ea79e633ff9b244e8a83e13e7189136c
gizmet <gizmet@protonmail.com>
2025-07-14 02:34:29 +0100

    Fix SHA256 hash for ghstats formula

--- ghstats.rb
+++ ghstats.rb
@@ -2,7 +2,7 @@ class Ghstats < Formula
   desc "GitHub contribution heatmap viewer for terminal"
   homepage "https://github.com/Gizmet/github-contribution-heatmap-viewer"
   url "https://github.com/Gizmet/github-contribution-heatmap-viewer/archive/v1.0.0.tar.gz"
-  sha256 "df9f51bfb4e1b001f3c795d28bae03273e1bb70e1ddcba8893bd16e763392b4"
+  sha256 "df9f51bfb4e1b001f3c795d28bae03273e1bb70e1ddcba8893bd16e763392b49"
   license "MIT"
   head "https://github.com/Gizmet/github-contribution-heatmap-viewer.git", branch: "master"
 
